5f9d8834ae7f5f44e1923696c0d83b48401d92e3
Gabriele Canepa <contact@gabrielecanepa.com>
2017-12-12 20:16:26 +0100

    Completed events basic concepts

--- src/browser.js
+++ src/browser.js
@@ -1,14 +1,2 @@
-// Console testing
-
 console.log("Hello from src/browser.js");
 console.log("A new line"); // autorefreshing in the browser console
-
-// Event listener
-
-const romain = document.getElementById("romain");
-
-romain.addEventListener("click", (event) => {
-  console.log(event);
-  console.log(event.target);
-  event.target.classList.toggle("img-circle");
-});
