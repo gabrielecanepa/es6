0cceee726a8714631fd4f1ae2ead4f0266d663a8
Gabriele Canepa <contact@gabrielecanepa.com>
2017-12-12 19:30:36 +0100

    Added event listener example

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,9 @@
 console.log("Hello from src/browser.js");
 
 console.log("A new line"); // autorefreshing in the browser console
+
+const romain = document.getElementById("romain");
+
+romain.addEventListener("click", (event) => {
+  console.log(event);
+});
